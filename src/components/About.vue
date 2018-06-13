30450fa60b2442bcdb02fe18e8b531ce95658f69
beigid <beigid@seattleu.edu>
2018-06-13 00:58:55 -0700

    Adjusted text styling for about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="about">
   <h1> About Page </h1>
- <p>
- 
-    </p>
     <p>
     The NASA API is here to supply you with all the images you love about space and time! This page pulls in the API data from the NASA website and allows search results to be generated right onto the page.
     </p>
     <p>
-    Check out the  <router-link to="/">search</router-link>
-  <router-view></router-view>  page to type in your favorite space query to see your results generate!
+    Check out the <router-link to="/">search page</router-link>
+  <router-view></router-view>to type in your favorite space query to see your results generate!
     </p>
     
 
@@ -24,6 +21,35 @@ h1 {
 
 p {
   color: white;
+  width:50%;
+  margin: 0 auto;
+  padding-bottom:30px;
+  text-shadow: 
+    -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000; 
+    
+
 }
+
+p a {
+text-decoration: underline;
+color: white;
+  width:50%;
+  margin: 0 auto;
+  padding-bottom:30px;
+  text-shadow: 
+    -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+    }
+
+p a:hover {
+  color:blue;
+}
+
+
 
 </style>
